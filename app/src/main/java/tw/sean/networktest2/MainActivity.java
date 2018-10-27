9fb4eb984ad3ae050e82ee72c4bc1ca85ff274b4
Seanhhe <seanhhe@gmail.com>
2018-10-27 09:58:23 +0800

    AS_NetworkTest2_20181027

---
+++ app/src/main/java/tw/sean/networktest2/MainActivity.java
@@ -0,0 +1,76 @@
+package tw.sean.networktest2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ConnectivityManager cmgr;
+    private MyReceiver myReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cmgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        myReceiver = new MyReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(myReceiver,filter);
+
+    }
+
+    @Override
+    public void finish() {
+        unregisterReceiver(myReceiver);
+        super.finish();
+    }
+
+    public void test1(View view) {
+        NetworkInfo info = cmgr.getActiveNetworkInfo();
+
+        //有無連線的測試
+        boolean isConnected = info != null && info.isConnected();
+        Log.v("brad", "network :" + isConnected);
+        //有無連wifi的測試
+        NetworkInfo wifiInfo = cmgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        Log.v("brad", "wifi : " + wifiInfo.isConnected());
+
+    }
+
+    private boolean isNetworkActive(){
+        NetworkInfo info = cmgr.getActiveNetworkInfo();
+        return info != null && info.isConnected();
+    }
+
+    private class MyReceiver extends BroadcastReceiver {
+
+        //網路狀態監控的處理
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (!isNetworkActive()) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("網路訊息")
+                        .setMessage("目前網路狀態不穩定")
+                        .setPositiveButton("我知道了", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+            }
+        }
+    }
+}
